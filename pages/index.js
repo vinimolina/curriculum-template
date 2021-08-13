377e5c5e07b34fe38bcc59b781351bb25f4411fb
Vinicius <vinicius.molina19@outlook.com>
2021-08-12 23:37:14 -0300

    Initial project

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Home = () => {
+    return (
+        <div>
+            <Head>
+                <title>HomePage</title>
+            </Head>
+
+            <main>
+                <h1>Hello World!</h1>
+            </main>
+        </div>
+    )
+}
+
+export default Home;
